eca701b0812a7de56743b2de26a4878977bbf5ff
Tom <blak1002@gmail.com>
2022-08-25 22:44:38 +0900

    [feat] LoginPage UI

--- android/BookChat/app/src/main/java/com/example/bookchat/activities/SplashActivity.kt
+++ android/BookChat/app/src/main/java/com/example/bookchat/activities/SplashActivity.kt
@@ -15,6 +15,7 @@ class SplashActivity : AppCompatActivity() {
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
+            finish()
         },2000)
 
     }
