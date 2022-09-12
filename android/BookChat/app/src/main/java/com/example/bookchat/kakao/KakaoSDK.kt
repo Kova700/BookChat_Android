d1d27ada1fd6afcb7a08c00dde557ce5cf6b9bf6
Tom <blak1002@gmail.com>
2022-09-12 15:32:24 +0900

    [feat] Kakao SDK Token available check feature add

--- android/BookChat/app/src/main/java/com/example/bookchat/kakao/KakaoSDK.kt
+++ android/BookChat/app/src/main/java/com/example/bookchat/kakao/KakaoSDK.kt
@@ -7,30 +7,17 @@ import com.kakao.sdk.auth.AuthApiClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.*
 import com.kakao.sdk.user.UserApiClient
+import kotlinx.coroutines.*
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class KakaoSDK(val context :Context) {
     private val userApiClient = UserApiClient.instance
     private val authApiClient = AuthApiClient.instance
 
-    //카카오 계정으로 로그인 예외 처리 (추후 콜백 -> 코루틴으로 리펙토링)
-    private val kakaoAcountCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
-        //if-else 구조 개선
-        Log.d(TAG, "kakaoAcountCallback 작동")
-        error?.let {
-            Log.d(TAG, "카카오계정으로 로그인 실패 error : $error")
-            kakaoLoginErrorHandler(error)
-        }
-        token?.let {
-            Log.d(TAG, "KakaoSDK: kakaoAcountCallback 카카오계정으로 로그인 성공 accessToken : ${token.accessToken}")
-            Log.d(TAG, "KakaoSDK: kakaoAcountCallback 카카오계정으로 로그인 성공 refreshToken : ${token.refreshToken}")
-            Log.d(TAG, "KakaoSDK: kakaoAcountCallback 카카오계정으로 로그인 성공 idToken : ${token.idToken}")
-        }
-    }
-
-    fun login(){
-        if (hasAccessToken()){
+    suspend fun login(){
+        if (isAvailableToken()){
             Log.d(TAG, "KakaoSDK: login() Token O- called")
-            bookChatLogin()
             return
         }
         Log.d(TAG, "KakaoSDK: login() Token x- called")
@@ -52,11 +39,9 @@ class KakaoSDK(val context :Context) {
         }
     }
 
-
-
     fun kakaoLogin() {
-        val canKakaoTalkLogin = userApiClient.isKakaoTalkLoginAvailable(context)
-        if (canKakaoTalkLogin) {
+        val isKakaoTalkLoginAvailable = userApiClient.isKakaoTalkLoginAvailable(context)
+        if (isKakaoTalkLoginAvailable) {
             Log.d(TAG, "KakaoSDK: kakaoLogin() KakaoTalk o- called")
             kakaoLoginWithKakaoTalk()
             return
@@ -65,53 +50,83 @@ class KakaoSDK(val context :Context) {
         kakaoLoginWithKakaoAccount()
     }
 
-    fun bookChatLogin(){
-        Log.d(TAG, "KakaoSDK: bookChatLogin() - called")
-    }
+    //매번 서버에게 요청을 보내기 전에 토큰 만료여부 확인
+    suspend fun isAvailableToken() :Boolean{
+        Log.d(TAG, "KakaoSDK: isAvailableToken() - called")
+        //accessTokenInfo호출할 때마다 토큰이 갱신된다하지만 확인해봐야함
 
-    fun hasAccessToken() :Boolean{
-        //단, hasToken()의 결과가 true라도 현재 사용자가 로그인 상태임을 보장하지 않습니다.
-        return authApiClient.hasToken() && isAvailableToken()
-    }
-
-    fun isAvailableToken() :Boolean{
-        var isAvailable = false
-        userApiClient.accessTokenInfo { tokenInfo, error ->
-            error?.let { tokenErrorHandler(error)}
-                ?: run { //토큰 유효성 체크 성공(필요 시 토큰 갱신됨)
-                    tokenInfo?.let {
-                        Log.i(TAG, "토큰 정보 보기 성공" +
-                                "\n회원번호: ${tokenInfo.id}" +
-                                "\n만료시간: ${tokenInfo.expiresIn} 초")
-                    }
-                    isAvailable = true
-                }
+        val result = suspendCancellableCoroutine<Boolean> { continuation ->
+            userApiClient.accessTokenInfo { tokenInfo, error ->
+                error?.let { tokenErrorHandler(error) }
+                tokenInfo?.let {
+                    Log.i(TAG, "토큰 정보" +
+                            "\n회원번호: ${tokenInfo.id}" +
+                            "\n만료시간: ${tokenInfo.expiresIn} 초" +
+                            "\n앱 ID: ${tokenInfo.appId}"
+                    )
+                    continuation.resume(tokenInfo.expiresIn > 0)
+                } ?: continuation.resume(false)
+            }
         }
-        return isAvailable
+        return result
     }
+
     fun tokenErrorHandler(error :Throwable){
+        Log.d(TAG, "KakaoSDK: tokenErrorHandler() - error : $error ")
         when{
             isInvalidToken(error) ->{
                 //로그인 필요
-                kakaoLogin()
+                Log.d(TAG, "KakaoSDK: tokenErrorHandler() - called (토큰 사용불가 혹은 토큰 없음)")
+//                kakaoLogin()
             }
             else -> {}
         }
     }
+    //카카오톡 로그인 성공하면 끝
+    //카카오톡 로그인 실패하면 에러 핸들링 할 거 있으면 하고 없으면 카카오 계정 로그인 시도
+    //카카오 계정 로그인 성공하면 끝
+    //카카오 계정 로그인 실패하면
+    //로그인 실패로 에러 핸들링
+
+    //로그인 성공해서 토큰을 서버에 넘겨줬는데 가입된 계저이면 유저 정보 가져오고
+    //가입되지 않은 계정이면 회원가입 진행
+
+    //로그인 버튼을 눌렀다.
+    //유효한 토큰이 있으면 서버로 토큰을 보내 유저 정보를 가져온다.
+    //유효한 토큰이 없다면 카카오로부터 다시 토큰을 받아온다.
+    //서버한테 토큰을 보내 유저 정보를 다시 가져온다.
+
+    //서버에게 매 요청시 토큰 만료시간 확인 하고 요청 보냄
+
+    //토큰 유효성 검증 부분
+    //자동으로 SDK에서 검증해준다는게 뭔말일까
+
 
     fun kakaoLoginWithKakaoTalk(){
-        Log.d(TAG, "KakaoSDK: kakaoLoginWithKakaoTalk()1 - called")
         userApiClient.loginWithKakaoTalk(context) { token, error ->
-            Log.d(TAG, "KakaoSDK: kakaoLoginWithKakaoTalk()2 - called")
-            //if-else 대체재 찾기
             error?.let {
                 kakaoLoginErrorHandler(error)
                 kakaoLoginWithKakaoAccount()
             }
-            token?.let { Log.d(TAG, "TestActivity: onCreate() 카카오톡으로 로그인 성공 (${token.accessToken})- called") }
+            token?.let {
+                Log.d(TAG, "TestActivity: onCreate() 카카오톡으로 로그인 성공 (${token.accessToken})- called")
+                //토큰 저장 성공
+            }
         }
     }
 
+    //카카오 계정으로 로그인 예외 처리 (추후 콜백 -> 코루틴으로 리펙토링)
+    private val kakaoAcountCallback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
+        error?.let {
+            Log.d(TAG, "카카오계정으로 로그인 실패 error : $error")
+            kakaoLoginErrorHandler(error)
+        }
+        token?.let {
+            Log.d(TAG, "KakaoSDK: kakaoAcountCallback 카카오계정으로 로그인 성공 accessToken : ${token.accessToken}")
+            Log.d(TAG, "KakaoSDK: kakaoAcountCallback 카카오계정으로 로그인 성공 refreshToken : ${token.refreshToken}")
+            Log.d(TAG, "KakaoSDK: kakaoAcountCallback 카카오계정으로 로그인 성공 idToken : ${token.idToken}")
+        }
+    }
 
     fun kakaoLoginWithKakaoAccount(){
         Log.d(TAG, "KakaoSDK: kakaoLoginWithKakaoAccount() - called")
