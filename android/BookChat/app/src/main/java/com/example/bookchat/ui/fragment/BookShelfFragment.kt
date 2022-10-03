5a20dedcfbf0c3800f64561b07d3260db0915a29
Tom <blak1002@gmail.com>
2022-10-04 04:39:02 +0900

    [feat] fragmet base Setting + HomeFragmet set

---
+++ android/BookChat/app/src/main/java/com/example/bookchat/ui/fragment/BookShelfFragment.kt
@@ -0,0 +1,25 @@
+package com.example.bookchat.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.bookchat.R
+import com.example.bookchat.databinding.FragmentBookShelfBinding
+
+class BookShelfFragment : Fragment() {
+
+    lateinit var binding : FragmentBookShelfBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_book_shelf,container,false)
+        return binding.root
+    }
+
+}
