854c9edcae0d46e953ec55210476a838f50710bc
Tom <blak1002@gmail.com>
2022-10-12 18:27:07 +0900

    [BookChat Repository split Android and Backend]

--- back/src/main/java/toy/bookchat/bookchat/domain/book/service/BookSearchService.java
+++
@@ -1,8 +0,0 @@
-package toy.bookchat.bookchat.domain.book.service;
-
-import toy.bookchat.bookchat.domain.book.dto.BookSearchRequestDto;
-import toy.bookchat.bookchat.domain.book.dto.BookSearchResponseDto;
-
-public interface BookSearchService {
-    BookSearchResponseDto searchByQuery(BookSearchRequestDto bookSearchRequestDto);
-}
