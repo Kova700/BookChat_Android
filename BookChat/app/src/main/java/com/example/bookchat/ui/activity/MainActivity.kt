cfa0305e74c96c8853c716d5db200cb2960eb8c9
Tom <blak1002@gmail.com>
2022-10-12 19:39:49 +0900

    [refector] MainActivit regercy code delete

--- BookChat/app/src/main/java/com/example/bookchat/ui/activity/MainActivity.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/activity/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.example.bookchat.ui.activity
 
-import android.content.DialogInterface
 import android.os.Bundle
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.bookchat.R
 import com.example.bookchat.databinding.ActivityMainBinding
 import com.example.bookchat.ui.fragment.*
-import com.example.bookchat.utils.Constants.TOKEN_PATH
-import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
@@ -62,56 +58,6 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.main_frame, fragment)
             .commit()
-    }
-
-/*아래 삭제 예정*/
-    private fun changeFragment(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.main_frame, fragment)
-            .commit()
-    }
-
-    fun clickMenu() {
-        with(binding){
-//            if(drawerlayout.isDrawerOpen(Gravity.RIGHT)) {
-//                drawerlayout.closeDrawer(Gravity.RIGHT)
-//                return
-//            }
-//            drawerlayout.openDrawer(Gravity.RIGHT)
-        }
-    }
-//    fun changePage(activityType: ActivityType) {
-//        val targetActivity = when(activityType) {
-//            ActivityType.bookShelfActivity -> { BookShelfActivity::class.java }
-//            ActivityType.searchActivity -> { SearchActivity::class.java }
-//        }
-//        val intent = Intent(this, targetActivity)
-//        startActivity(intent)
-//    }
-    fun clickSignOut(){
-        val dialog = AlertDialog.Builder(this,android.R.style.Theme_DeviceDefault_Light_Dialog)
-        dialog.setTitle("정말 로그아웃하시겠습니까?")
-            .setPositiveButton("취소",object : DialogInterface.OnClickListener {
-                override fun onClick(dialog: DialogInterface?, which: Int) {
-                }
-            })
-            .setNeutralButton("로그아웃",object : DialogInterface.OnClickListener{
-                override fun onClick(dialog: DialogInterface?, which: Int) {
-                    deleteToken()
-                    finish()
-                }
-            })
-            .setCancelable(true) //백버튼으로 닫히게 설정
-            .show()
-    }
-    fun deleteToken(){
-        val token = File(TOKEN_PATH)
-        if (token.exists()) token.delete()
-    }
-    private fun getUserInfo(){
-//        binding.profile.clipToOutline = true //프로필 라운딩
-//        binding.userModel?.activityInitialization()
     }
 
 }
