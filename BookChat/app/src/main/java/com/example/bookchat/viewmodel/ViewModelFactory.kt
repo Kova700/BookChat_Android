1b052d8e678adec245c9a0546cfbc597901312a7
Tom <blak1002@gmail.com>
2022-10-14 16:06:01 +0900

    [refector] delete Legacy Code

--- BookChat/app/src/main/java/com/example/bookchat/viewmodel/ViewModelFactory.kt
+++ BookChat/app/src/main/java/com/example/bookchat/viewmodel/ViewModelFactory.kt
@@ -2,12 +2,10 @@ package com.example.bookchat.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.bookchat.repository.BookRepository
 import com.example.bookchat.repository.UserRepository
 
 // 뷰모델에 인자를 넘겨주기 위한 팩토리 메서드
 class ViewModelFactory() : ViewModelProvider.Factory {
-    private lateinit var bookRepository : BookRepository
     private lateinit var userRepository : UserRepository
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
@@ -16,11 +14,6 @@ class ViewModelFactory() : ViewModelProvider.Factory {
             modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
                 userRepository = UserRepository()
                 return HomeViewModel(userRepository) as T
-            }
-
-            modelClass.isAssignableFrom(SearchResultViewModel::class.java) -> {
-                bookRepository = BookRepository()
-                return SearchResultViewModel(bookRepository) as T
             }
 
             modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
