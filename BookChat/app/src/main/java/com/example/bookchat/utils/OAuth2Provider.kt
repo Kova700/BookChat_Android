6c585bc52e7f2fe659b3ccd34f0a204dc5eaca22
Tom <blak1002@gmail.com>
2022-10-16 15:03:21 +0900

    [chore] SerializedName change

--- BookChat/app/src/main/java/com/example/bookchat/utils/OAuth2Provider.kt
+++ BookChat/app/src/main/java/com/example/bookchat/utils/OAuth2Provider.kt
@@ -2,9 +2,7 @@ package com.example.bookchat.utils
 
 import com.google.gson.annotations.SerializedName
 
-enum class OAuth2Provider(val provider: String){
-    @SerializedName("GOOGLE")
-    GOOGLE("google"),
-    @SerializedName("KAKAO")
-    KAKAO("kakao")
+enum class OAuth2Provider{
+    @SerializedName("google")GOOGLE ,
+    @SerializedName("kakao") KAKAO
 }
