f0ad1da80a29dee30ddb9352ca6f21afc61c8759
Tom <blak1002@gmail.com>
2022-10-17 05:56:38 +0900

    [chore] delete comment in UserSignUpdto

--- BookChat/app/src/main/java/com/example/bookchat/data/UserSignUpDto.kt
+++ BookChat/app/src/main/java/com/example/bookchat/data/UserSignUpDto.kt
@@ -1,12 +1,14 @@
 package com.example.bookchat.data
 
+import com.example.bookchat.utils.OAuth2Provider
 import com.example.bookchat.utils.ReadingTaste
 import okhttp3.MultipartBody
 import java.io.Serializable
 
 data class UserSignUpDto(
+    val oauth2Provider : OAuth2Provider? = null,
     var nickname: String = "",
-    val defaultProfileImageType : Int, //등록 이미지 없을 시 기본이미지 노출하게 (기본 이미지 번호)
-    var userProfileImage : MultipartBody.Part? = null,
-    var readingTastes : List<ReadingTaste> = arrayListOf()
+    val defaultProfileImageType : Int? = null,
+    var readingTastes : List<ReadingTaste> = arrayListOf(),
+    var userProfileImage : MultipartBody.Part? = null
 ) : Serializable
