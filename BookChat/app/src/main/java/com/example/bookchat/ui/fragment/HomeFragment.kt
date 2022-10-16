a282f25132d38e30713557f6376f4205bab157ed
Tom <blak1002@gmail.com>
2022-10-17 04:57:34 +0900

    [feat] HomeFragment temporary init logic

--- BookChat/app/src/main/java/com/example/bookchat/ui/fragment/HomeFragment.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/fragment/HomeFragment.kt
@@ -1,22 +1,28 @@
 package com.example.bookchat.ui.fragment
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bookchat.R
 import com.example.bookchat.adapter.MainChatRoomAdapter
 import com.example.bookchat.databinding.FragmentHomeBinding
+import com.example.bookchat.ui.activity.LoginActivity
+import com.example.bookchat.ui.activity.MainActivity
+import com.example.bookchat.viewmodel.HomeViewModel
+import com.example.bookchat.viewmodel.ViewModelFactory
 
-//이미 살아있는 Fragment 객체가 있으면 그 상태를 불러오는게 Best (스크롤 상태까지 그대로)
 
 class HomeFragment : Fragment() {
 
-    lateinit var binding :FragmentHomeBinding
+    private lateinit var binding :FragmentHomeBinding
+    private lateinit var homeViewModel: HomeViewModel
     private lateinit var chatRoomAdapter: MainChatRoomAdapter
 
     //프래그먼트가 액티비티에 붙을 때 호출됨
@@ -39,6 +45,20 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false)
+        homeViewModel = ViewModelProvider(this, ViewModelFactory()).get(HomeViewModel::class.java)
+        with(binding){
+            lifecycleOwner = this@HomeFragment
+            viewModel = homeViewModel
+        }
+        homeViewModel.goLoginActivityCallBack = {
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK //새로운 태스크 생성
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK) // 실행 액티비티 외 모두 제거
+            startActivity(intent)
+        }
+        homeViewModel.activityInitialization()
+
+
         initRecyclerView()
 
 
@@ -75,8 +95,6 @@ class HomeFragment : Fragment() {
             todayChatRoomListView.adapter = chatRoomAdapter
             todayChatRoomListView.setHasFixedSize(true)
             todayChatRoomListView.layoutManager = LinearLayoutManager(requireContext())
-//            val snapHelper = LinearSnapHelper()
-//            snapHelper.attachToRecyclerView(chatRoomRecyclerView)
         }
     }
 
