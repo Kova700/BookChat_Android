0a5ebf41d0fb637016bba190131aed314eaa4f80
Tom <blak1002@gmail.com>
2022-10-18 07:01:02 +0900

    [refector] Migration EventCallBack -> SharedFlow (SelectTasteActivity)

--- BookChat/app/src/main/java/com/example/bookchat/ui/activity/SelectTasteActivity.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/activity/SelectTasteActivity.kt
@@ -5,13 +5,17 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.bookchat.R
 import com.example.bookchat.data.UserSignUpDto
 import com.example.bookchat.databinding.ActivitySelectTasteBinding
 import com.example.bookchat.ui.activity.SignUpActivity.Companion.EXTRA_SIGNUP_DTO
 import com.example.bookchat.ui.activity.SignUpActivity.Companion.EXTRA_USER_PROFILE_BYTE_ARRAY2
 import com.example.bookchat.viewmodel.SelectTasteViewModel
+import com.example.bookchat.viewmodel.SelectTasteViewModel.SelectTasteEvent
 import com.example.bookchat.viewmodel.ViewModelFactory
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.launch
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -24,52 +28,55 @@ class SelectTasteActivity : AppCompatActivity() {
     private lateinit var binding :ActivitySelectTasteBinding
     private lateinit var selectTasteViewModel: SelectTasteViewModel
     private lateinit var signUpUserDto :UserSignUpDto
-    private var userProfileByteArray :ByteArray? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_select_taste)
         selectTasteViewModel = ViewModelProvider(this,ViewModelFactory()).get(SelectTasteViewModel::class.java)
-        selectTasteViewModel.goMainActivityCallBack = {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK //새로운 태스크 생성
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK) // 실행 액티비티 외 모두 제거
-            startActivity(intent)
-        }
+
         with(binding){
             lifecycleOwner = this@SelectTasteActivity
             activity = this@SelectTasteActivity
             viewModel = selectTasteViewModel
         }
+
+        lifecycleScope.launch {
+            selectTasteViewModel.eventFlow.collect { event -> handleEvent(event) }
+        }
+
         getSignUpUserDtoFromIntent()
         getSignUpUserProfileImgFromIntent()
-        getFinalSignUpUserDto()
     }
 
     private fun getSignUpUserDtoFromIntent(){
         signUpUserDto = intent.getSerializableExtra(EXTRA_SIGNUP_DTO) as UserSignUpDto
+        selectTasteViewModel._signUpDto.value = signUpUserDto
     }
 
     private fun getSignUpUserProfileImgFromIntent(){
         val bitmapByteArray = intent?.getByteArrayExtra(EXTRA_USER_PROFILE_BYTE_ARRAY2)
-        bitmapByteArray?.let { userProfileByteArray = bitmapByteArray }
-    }
-
-    private fun getFinalSignUpUserDto(){
-        val imageMultipartBody = userProfileByteArray?.let { byteArrayToRequestBody(it) }
+        val imageMultipartBody = bitmapByteArray?.let { byteArrayToRequestBody(it) }
         val userProfileImageMultiPart = imageMultipartBody?.let {
             MultipartBody.Part.createFormData(IMAGE_MULTIPART_NAME,IMAGE_FILE_NAME, it)
         }
         signUpUserDto.userProfileImage = userProfileImageMultiPart
-        selectTasteViewModel._signUpDto.value = signUpUserDto //임시
     }
 
     private fun byteArrayToRequestBody(byteArray : ByteArray) :RequestBody{
         return RequestBody.create(MediaType.parse(CONTENT_TYPE_IMAGE_WEBP),byteArray)
     }
 
-    fun clickBackBtn() {
-        finish()
+    private fun handleEvent(event: SelectTasteEvent) = when(event) {
+        is SelectTasteEvent.Forbidden -> { Snackbar.make(binding.selectTasteLayout,R.string.message_forbidden, Snackbar.LENGTH_SHORT).show() }
+        is SelectTasteEvent.NetworkError -> { Snackbar.make(binding.selectTasteLayout,R.string.message_error_network, Snackbar.LENGTH_SHORT).show() }
+        is SelectTasteEvent.UnknownError -> { Snackbar.make(binding.selectTasteLayout,R.string.message_error_else, Snackbar.LENGTH_SHORT).show() }
+        is SelectTasteEvent.MoveToBack -> { finish() }
+        is SelectTasteEvent.MoveToMain -> {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+        }
     }
 
 }
--- BookChat/app/src/main/java/com/example/bookchat/viewmodel/SelectTasteViewModel.kt
+++ BookChat/app/src/main/java/com/example/bookchat/viewmodel/SelectTasteViewModel.kt
@@ -9,52 +9,46 @@ import com.example.bookchat.R
 import com.example.bookchat.data.UserSignUpDto
 import com.example.bookchat.repository.UserRepository
 import com.example.bookchat.response.NetworkIsNotConnectedException
-import com.example.bookchat.response.ResponseBodyEmptyException
 import com.example.bookchat.response.TokenExpiredException
-import com.example.bookchat.response.UnauthorizedOrBlockedUserException
+import com.example.bookchat.response.ForbiddenException
 import com.example.bookchat.utils.Constants.TAG
 import com.example.bookchat.utils.ReadingTaste
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 
 class SelectTasteViewModel(private val userRepository : UserRepository) :ViewModel() {
-    lateinit var goMainActivityCallBack :() -> Unit
+    private val _eventFlow = MutableSharedFlow<SelectTasteEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
 
     private val selectedTastes = ArrayList<ReadingTaste>()
-    private val _isTastesEmpty = MutableStateFlow<Boolean>(true)
     private var recursiveChecker = false //임시 (구조 개선 필요)
 
-    var _signUpDto = MutableStateFlow<UserSignUpDto>(UserSignUpDto()) //초기값 임시
+    private val _isTastesEmpty = MutableStateFlow<Boolean>(true)
+    val isTastesEmpty = _isTastesEmpty.asStateFlow()
 
-    val isTastesEmpty : StateFlow<Boolean>
-        get() = _isTastesEmpty
+    val _signUpDto = MutableStateFlow<UserSignUpDto>(UserSignUpDto())
 
     fun signUp() = viewModelScope.launch {
         _signUpDto.value.readingTastes = selectedTastes
-        Log.d(TAG, "SelectTasteViewModel: signUp() - called : userDto : ${_signUpDto.value}")
-        runCatching{ _signUpDto.value?.let { userRepository.signUp(it) } } //!! 표시 임시
+        runCatching{ userRepository.signUp(_signUpDto.value) }
             .onSuccess { signIn() }
-            .onFailure { Log.d(TAG, "SelectTasteViewModel: signUp() - onFailure $it") }
+            .onFailure { failHandler(it) }
     }
 
     private fun signIn() = viewModelScope.launch {
         Log.d(TAG, "SelectTasteViewModel: signIn() - called")
         runCatching { userRepository.signIn() }
             .onSuccess { requestUserInfo() }
-            .onFailure {
-                Log.d(TAG, "SelectTasteViewModel: signIn() - onFailure ")
-                when(it){
-                    is UnauthorizedOrBlockedUserException -> Toast.makeText(App.instance.applicationContext,"차단된 사용자 입니다.\n24시간 후에 다시 시도해주세요.", Toast.LENGTH_SHORT).show()
-                    else -> Log.d(TAG, "LoginViewModel: bookchatLogin() - elseException : $it")
-                }
-            }
+            .onFailure { failHandler(it) }
     }
 
     private fun requestUserInfo() = viewModelScope.launch {
         Log.d(TAG, "LoginViewModel: requestUserInfo() - called")
         runCatching{ userRepository.getUserProfile() }
-            .onSuccess { goMainActivityCallBack() }
+            .onSuccess { startEvent(SelectTasteEvent.MoveToMain) }
             .onFailure { failHandler(it) }
     }
 
@@ -62,24 +56,7 @@ class SelectTasteViewModel(private val userRepository : UserRepository) :ViewMod
         Log.d(TAG, "LoginViewModel: requestTokenRenewal() - called")
         runCatching{ userRepository.requestTokenRenewal() }
             .onSuccess { if (recursiveChecker == false) requestUserInfo(); recursiveChecker = true }
-    }
-
-    //Status Code별 Exception handle
-    private fun failHandler(exception: Throwable) {
-        when(exception){
-            is TokenExpiredException -> requestTokenRenewal()
-            is UnauthorizedOrBlockedUserException -> {
-                Log.d(TAG, "LoginViewModel: failHandler() - unauthorizedOrBlockedUserException")
-            }
-            is ResponseBodyEmptyException -> {
-                Log.d(TAG, "LoginViewModel: failHandler() - ResponseBodyEmptyException")
-            }
-            is NetworkIsNotConnectedException -> {
-                Toast.makeText(App.instance.applicationContext, R.string.message_error_network_toast, Toast.LENGTH_SHORT).show()
-            }
-            else -> {
-                Log.d(TAG, "LoginViewModel: failHandler() - Exception : $exception")}
-        }
+            .onFailure { failHandler(it) }
     }
 
     fun clickTaste(pickedReadingTaste: ReadingTaste) {
@@ -95,5 +72,30 @@ class SelectTasteViewModel(private val userRepository : UserRepository) :ViewMod
             return
         }
         _isTastesEmpty.value = false
+    }
+    fun clickBackBtn() {
+        startEvent(SelectTasteEvent.MoveToBack)
+    }
+
+    private fun startEvent (event : SelectTasteEvent) = viewModelScope.launch {
+        _eventFlow.emit(event)
+    }
+
+    sealed class SelectTasteEvent {
+        object MoveToMain :SelectTasteEvent()
+        object Forbidden : SelectTasteEvent()
+        object NetworkError : SelectTasteEvent()
+        object UnknownError : SelectTasteEvent()
+        object MoveToBack : SelectTasteEvent()
+    }
+
+    private fun failHandler(exception: Throwable) {
+        Log.d(TAG, "SelectTasteViewModel: failHandler() - called")
+        when(exception){
+            is TokenExpiredException -> requestTokenRenewal()
+            is ForbiddenException -> startEvent(SelectTasteEvent.Forbidden)
+            is NetworkIsNotConnectedException -> startEvent(SelectTasteEvent.NetworkError)
+            else -> startEvent(SelectTasteEvent.UnknownError)
+        }
     }
 }
