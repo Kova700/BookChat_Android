8a69bcb14b02c2be81d35e1e272c5295dd1431be
Tom <blak1002@gmail.com>
2022-10-17 17:48:31 +0900

    [fix] change string constant

--- BookChat/app/src/main/java/com/example/bookchat/adapter/DataBindingAdapter.kt
+++ BookChat/app/src/main/java/com/example/bookchat/adapter/DataBindingAdapter.kt
@@ -63,19 +63,19 @@ object DataBindingAdapter {
             }
             NameCheckStatus.IsShort -> {
                 textView.setTextColor(Color.parseColor("#FF004D"))
-                textView.text = R.string.message_name_check_status_short.toString()
+                textView.text = App.instance.resources.getString(R.string.message_name_check_status_short)
             }
             NameCheckStatus.IsDuplicate ->{
                 textView.setTextColor(Color.parseColor("#FF004D"))
-                textView.text = R.string.message_name_check_status_duplicate.toString()
+                textView.text = App.instance.resources.getString(R.string.message_name_check_status_duplicate)
             }
             NameCheckStatus.IsSpecialCharInText -> {
                 textView.setTextColor(Color.parseColor("#FF004D"))
-                textView.text = R.string.message_name_check_status_special_char.toString()
+                textView.text = App.instance.resources.getString(R.string.message_name_check_status_special_char)
             }
             NameCheckStatus.IsPerfect -> {
                 textView.setTextColor(Color.parseColor("#5648FF"))
-                textView.text = R.string.message_name_check_status_perfect.toString()
+                textView.text = App.instance.resources.getString(R.string.message_name_check_status_perfect)
             }
         }
     }
