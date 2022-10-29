7df7bb80741ab432e84003843f1c27add05bc032
Tom <blak1002@gmail.com>
2022-10-30 05:51:49 +0900

    [feat] automatically change fragments based on searchTap status

--- BookChat/app/src/main/java/com/example/bookchat/ui/fragment/SearchTapHistoryFragment.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/fragment/SearchTapHistoryFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.bookchat.R
+import com.example.bookchat.databinding.FragmentSearchTapHistoryBinding
 
 /*추후 확장이 필요하다 느끼면 ViewModel로 로직 이동*/
 class SearchTapHistoryFragment : Fragment() {
+    private lateinit var binding : FragmentSearchTapHistoryBinding
     private val tempData = listOf("이펙티브", "인간관계론", "UI/UX")
 
     override fun onCreateView(
@@ -16,7 +20,10 @@ class SearchTapHistoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         //최근 검색어 받아와야함
-        
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search_tap_history, container, false)
+
+        //아이템 연결하고 클릭 이벤트 연결해야함
+
+        return binding.root
     }
 }
--- BookChat/app/src/main/java/com/example/bookchat/viewmodel/ViewModelFactory.kt
+++ BookChat/app/src/main/java/com/example/bookchat/viewmodel/ViewModelFactory.kt
@@ -2,33 +2,35 @@ package com.example.bookchat.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.bookchat.repository.BookRepository
 import com.example.bookchat.repository.UserRepository
 
 // 뷰모델에 인자를 넘겨주기 위한 팩토리 메서드
 class ViewModelFactory() : ViewModelProvider.Factory {
-    private lateinit var userRepository : UserRepository
+    private val userRepository by lazy { UserRepository() }
+    private val bookRepository by lazy { BookRepository() }
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
 
         when{
             modelClass.isAssignableFrom(HomeViewModel::class.java) -> {
-                userRepository = UserRepository()
                 return HomeViewModel(userRepository) as T
             }
 
             modelClass.isAssignableFrom(LoginViewModel::class.java) -> {
-                userRepository = UserRepository()
                 return LoginViewModel(userRepository) as T
             }
 
             modelClass.isAssignableFrom(SelectTasteViewModel::class.java) -> {
-                userRepository = UserRepository()
                 return SelectTasteViewModel(userRepository) as T
             }
 
             modelClass.isAssignableFrom(SignUpViewModel::class.java) -> {
-                userRepository = UserRepository()
                 return SignUpViewModel(userRepository) as T
+            }
+
+            modelClass.isAssignableFrom(SearchViewModel::class.java) -> {
+                return SearchViewModel(bookRepository) as T
             }
 
             else -> throw IllegalArgumentException("unknown model class : ${modelClass}")
