6776beb9a9285e76c6099958e992951ee3423f9f
Tom <blak1002@gmail.com>
2022-10-31 23:04:12 +0900

    [feat] searchResult Ui temporary Setting

--- BookChat/app/src/main/java/com/example/bookchat/ui/fragment/SearchTapResultFragment.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/fragment/SearchTapResultFragment.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.bookchat.R
+import com.example.bookchat.adapter.MainChatRoomAdapter
+import com.example.bookchat.adapter.WishBookTabAdapter
+import com.example.bookchat.databinding.FragmentSearchTapResultBinding
 
 /*추후 확장이 필요하다 느끼면 ViewModel로 로직 이동*/
 class SearchTapResultFragment : Fragment() {
+    private lateinit var binding :FragmentSearchTapResultBinding
+
+    private lateinit var chatRoomAdapter: MainChatRoomAdapter // 임시
+    private lateinit var wishBookAdapter : WishBookTabAdapter // 임시
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,7 +26,22 @@ class SearchTapResultFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         //검색결과 가져와야함
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search_tap_result, container, false)
+        initRecyclerView()
+        return binding.root
+    }
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+    private fun initRecyclerView(){
+        with(binding){
+            chatRoomAdapter = MainChatRoomAdapter()
+            searchResultChatRoomRcv.adapter = chatRoomAdapter
+            searchResultChatRoomRcv.setHasFixedSize(true)
+            searchResultChatRoomRcv.layoutManager = LinearLayoutManager(requireContext())
+
+            wishBookAdapter = WishBookTabAdapter()
+            searchResultBookRcv.adapter = wishBookAdapter
+            searchResultBookRcv.setHasFixedSize(true)
+            searchResultBookRcv.layoutManager = GridLayoutManager(requireContext(),3) //중앙정렬 해야함 Or 개수 화면에 따라 늘어나게 설정
+        }
     }
 }
--- BookChat/app/src/main/java/com/example/bookchat/viewmodel/SearchViewModel.kt
+++ BookChat/app/src/main/java/com/example/bookchat/viewmodel/SearchViewModel.kt
@@ -2,10 +2,18 @@ package com.example.bookchat.viewmodel
 
 import android.text.Editable
 import android.text.TextWatcher
+import android.widget.Toast
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.example.bookchat.App
+import com.example.bookchat.data.Book
 import com.example.bookchat.repository.BookRepository
 import com.example.bookchat.utils.SearchTapStatus
 import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 class SearchViewModel(private var bookRepository :BookRepository) :ViewModel() {
 
@@ -13,6 +21,8 @@ class SearchViewModel(private var bookRepository :BookRepository) :ViewModel() {
     //val searchTapStatus = _searchTapStatus.asStateFlow()
 
     val _searchKeyWord = MutableStateFlow<String>("")
+
+//    lateinit var pagingSearchResultBook :Flow<PagingData<Book>>
 
     val editTextWatcher = object : TextWatcher {
         override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
@@ -28,6 +38,27 @@ class SearchViewModel(private var bookRepository :BookRepository) :ViewModel() {
             return
         }
         _searchTapStatus.value = SearchTapStatus.Searching
+    }
+
+    fun clickSearchbtn() = viewModelScope.launch {
+        val keyword = _searchKeyWord.value
+        if (keyword.isEmpty()) {
+            Toast.makeText(App.instance.applicationContext,"검색어를 입력해주세요.",Toast.LENGTH_SHORT).show() //임시
+            return@launch
+        }
+        _searchTapStatus.value = SearchTapStatus.Result
+//        searchBooks(keyword) //페이징 시작
+//        searchChatRoom(keyword) //페이징 시작
+    }
+
+
+    private suspend fun searchBooks(keyword :String) {
+        //각자 책부분 채팅부분 로딩 프로그래스바 만들어서 서로 로딩상태 두개로 구분 해서 결과 기다리는 표시하기
+//       runCatching { bookRepository.searchBooks(keyword) }
+    }
+
+    private suspend fun searchChatRoom(keyword :String){
+//        runCatching { bookRepository.searchChatRoom(keyword) }
     }
 
 
