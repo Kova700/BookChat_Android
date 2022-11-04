97f5c87d46c41a3bc6a4cc8b81964ecebdbda35d
Tom <blak1002@gmail.com>
2022-11-04 17:38:21 +0900

    [chore] modify  BookSearchResult name

--- BookChat/app/src/main/java/com/example/bookchat/api/BookApiInterface.kt
+++ BookChat/app/src/main/java/com/example/bookchat/api/BookApiInterface.kt
@@ -1,6 +1,7 @@
 package com.example.bookchat.api
 
-import com.example.bookchat.data.BookSearchResultDto
+import com.example.bookchat.data.BookSearchResult
+import com.example.bookchat.utils.BookSearchSortOption
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -9,13 +10,12 @@ interface BookApiInterface {
     //API 테스트
     //@POST("/3862dd02-166b-4975-9a3c-1666ebd4fcfa")
 
-    //제목으로 도서 검색 => 통합 쿼리 검색 api로 수정해야함
     @GET("/v1/api/books")
     suspend fun getBookFromTitle(
-        @Query("title") title:String,
+        @Query("query") query:String,
         @Query("size") size:String,
         @Query("page") page:String,
-        @Query("sort") sort:String,
-    ): Response<BookSearchResultDto>
+        @Query("sort") sort: BookSearchSortOption = BookSearchSortOption.LATEST,
+    ): Response<BookSearchResult>
 
 }
