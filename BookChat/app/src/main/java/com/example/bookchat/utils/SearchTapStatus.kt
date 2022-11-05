4b6b2d30042a76e66fdb124cade4c732e5eb719f
Tom <blak1002@gmail.com>
2022-11-06 00:33:41 +0900

    [feat] simpleSearchBook Loading Ui

--- BookChat/app/src/main/java/com/example/bookchat/utils/SearchTapStatus.kt
+++ BookChat/app/src/main/java/com/example/bookchat/utils/SearchTapStatus.kt
@@ -1,10 +1,9 @@
 package com.example.bookchat.utils
 
-sealed class SearchTapStatus{
-    object Default :SearchTapStatus() //추천검색어
-    object History :SearchTapStatus() //최근 검색어
-    object Searching :SearchTapStatus() //관련 도서명
-    object Loading :SearchTapStatus() //isLoading
-    object Result :SearchTapStatus() //ShowResult
-    object Detail :SearchTapStatus() //ShowResult
+sealed class SearchTapStatus {
+    object Default : SearchTapStatus() //추천검색어
+    object History : SearchTapStatus() //최근 검색어
+    object Searching : SearchTapStatus() //관련 도서명
+    object Result : SearchTapStatus() //ShowResult
+    object Detail : SearchTapStatus() //ShowResult
 }
