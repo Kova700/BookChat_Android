9d0b86d2b99c756a6bb6349ec30b1a478a9954c5
Tom <blak1002@gmail.com>
2022-11-06 17:18:06 +0900

    [feat] registerBookStatus API base Setting

---
+++ BookChat/app/src/main/java/com/example/bookchat/data/RequestRegisterWishBook.kt
@@ -0,0 +1,19 @@
+package com.example.bookchat.data
+
+import com.example.bookchat.utils.ReadingStatus
+import com.google.gson.annotations.SerializedName
+
+data class RequestRegisterWishBook(
+    @SerializedName("isbn")
+    val isbn :String,
+    @SerializedName("title")
+    val title :String,
+    @SerializedName("authors")
+    val authors :List<String>,
+    @SerializedName("publisher")
+    val publisher :String,
+    @SerializedName("bookCoverImageUrl")
+    val bookCoverImageUrl :String,
+    @SerializedName("readingStatus")
+    val readingStatus :ReadingStatus = ReadingStatus.WISH
+)
---
+++ BookChat/app/src/main/java/com/example/bookchat/utils/StarRating.kt
@@ -0,0 +1,12 @@
+package com.example.bookchat.utils
+
+import com.google.gson.annotations.SerializedName
+
+enum class starRating {
+    @SerializedName("ZERO") ZERO, @SerializedName("HALF") HALF,
+    @SerializedName("ONE") ONE, @SerializedName("ONE_HALF") ONE_HALF,
+    @SerializedName("TWO") TWO, @SerializedName("TWO_HALF") TWO_HALF,
+    @SerializedName("THREE") THREE, @SerializedName("THREE_HALF") THREE_HALF,
+    @SerializedName("FOUR") FOUR, @SerializedName("FOUR_HALF") FOUR_HALF,
+    @SerializedName("FIVE") FIVE
+}
