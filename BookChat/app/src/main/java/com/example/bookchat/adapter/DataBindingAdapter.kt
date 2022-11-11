32ca36a2a9aab49d5a2f291d7fa40f2692a69d0d
Tom <blak1002@gmail.com>
2022-11-12 01:44:10 +0900

    [feat] child Fragment BackStackEvent

--- BookChat/app/src/main/java/com/example/bookchat/adapter/DataBindingAdapter.kt
+++ BookChat/app/src/main/java/com/example/bookchat/adapter/DataBindingAdapter.kt
@@ -120,13 +120,35 @@ object DataBindingAdapter {
 
     /**검색창 : 검색어 삭제버튼 Visible 설정*/
     @JvmStatic
-    @BindingAdapter("setSearchDeleteBtnVisible")
-    fun setSearchDeleteBtnVisible(view: View, searchTapStatus : SearchTapStatus){
+    @BindingAdapter("setSearchDeleteBtnVisibility")
+    fun setSearchDeleteBtnVisibility(view: View, searchTapStatus : SearchTapStatus){
         if (searchTapStatus !is SearchTapStatus.Searching) {
             view.visibility = View.INVISIBLE
             return
         }
         view.visibility = View.VISIBLE
+    }
+
+    /**검색창 애니메이션 터치뷰 : 검색창 애니메이션 터치뷰 Visible 설정*/
+    @JvmStatic
+    @BindingAdapter("setAnimationTouchViewVisibility")
+    fun setAnimationTouchViewVisibility(view: View, searchTapStatus : SearchTapStatus){
+        if (searchTapStatus !is SearchTapStatus.Default) {
+            view.visibility = View.INVISIBLE
+            return
+        }
+        view.visibility = View.VISIBLE
+    }
+
+    /**검색창 뒤로가기 버튼 : 검색창 뒤로가기 버튼 Visible 설정*/
+    @JvmStatic
+    @BindingAdapter("setBackBtnViewVisibility")
+    fun setBackBtnViewVisibility(view: View, searchTapStatus : SearchTapStatus){
+        if (searchTapStatus !is SearchTapStatus.Default) {
+            view.visibility = View.VISIBLE
+            return
+        }
+        view.visibility = View.INVISIBLE
     }
 
     /**리사이클러뷰 아이템 연결*/
