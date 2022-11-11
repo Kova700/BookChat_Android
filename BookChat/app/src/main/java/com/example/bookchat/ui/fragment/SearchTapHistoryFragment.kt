21b3bc61c990e7138ec53f0190c44e28bd988e1a
Tom <blak1002@gmail.com>
2022-11-12 02:09:39 +0900

    [feat] searchHistory click Event set

--- BookChat/app/src/main/java/com/example/bookchat/ui/fragment/SearchTapHistoryFragment.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/fragment/SearchTapHistoryFragment.kt
@@ -4,20 +4,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bookchat.R
 import com.example.bookchat.adapter.SearchHistoryAdapter
 import com.example.bookchat.databinding.FragmentSearchTapHistoryBinding
 import com.example.bookchat.utils.DataStoreManager
+import com.example.bookchat.viewmodel.SearchViewModel
+import com.example.bookchat.viewmodel.ViewModelFactory
 import kotlinx.coroutines.launch
 
 /*추후 확장이 필요하다 느끼면 ViewModel로 로직 이동*/
 class SearchTapHistoryFragment : Fragment() {
     private lateinit var binding : FragmentSearchTapHistoryBinding
     private lateinit var searchHistoryAdapter :SearchHistoryAdapter
+    private lateinit var searchViewModel: SearchViewModel
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,6 +33,7 @@ class SearchTapHistoryFragment : Fragment() {
     ): View? {
         //최근 검색어 받아와야함
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search_tap_history, container, false)
+        searchViewModel = ViewModelProvider(requireParentFragment(), ViewModelFactory()).get(SearchViewModel::class.java)
         binding.fragment = this
         initAdapter()
         initRcv()
@@ -38,7 +46,8 @@ class SearchTapHistoryFragment : Fragment() {
         searchHistoryAdapter.setItemClickListener(object :
             SearchHistoryAdapter.OnItemClickListener {
             override fun onClick(v: View, position: Int) {
-                //해당 검색어로 검색
+                val keyword = (v as TextView).text.toString()
+                searchViewModel.clickHistory(keyword)
             }
         })
     }
