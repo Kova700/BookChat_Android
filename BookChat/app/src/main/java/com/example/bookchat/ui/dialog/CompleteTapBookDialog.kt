db34756619c0edc9ce8d92f5eee949c02d400a2b
Tom <blak1002@gmail.com>
2022-12-04 13:21:52 +0900

    [feat] openAgonizeAcitivty in completeBookTapDialog

--- BookChat/app/src/main/java/com/example/bookchat/ui/dialog/CompleteTapBookDialog.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/dialog/CompleteTapBookDialog.kt
@@ -1,5 +1,6 @@
 package com.example.bookchat.ui.dialog
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
@@ -9,11 +10,15 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.bookchat.R
 import com.example.bookchat.data.BookShelfItem
 import com.example.bookchat.databinding.DialogCompleteBookTapClickedBinding
+import com.example.bookchat.ui.activity.AgonizeHistoryActivity
 import com.example.bookchat.viewmodel.CompleteBookTapDialogViewModel
+import com.example.bookchat.viewmodel.CompleteBookTapDialogViewModel.CompleteBookEvent
 import com.example.bookchat.viewmodel.ViewModelFactory
+import kotlinx.coroutines.launch
 
 class CompleteTapBookDialog(private val book: BookShelfItem) : DialogFragment() {
     private lateinit var binding : DialogCompleteBookTapClickedBinding
@@ -33,11 +38,29 @@ class CompleteTapBookDialog(private val book: BookShelfItem) : DialogFragment() 
         completeBookTapDialogViewModel.book = book
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        observeEventFlow()
+
+        //고민기록 연결
+        //독후감 만들고 연결
+
+        return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun observeEventFlow() {
+        lifecycleScope.launch{
+            completeBookTapDialogViewModel.eventFlow.collect { event -> handleEvent(event) }
+        }
+    }
+
+    private fun handleEvent(event :CompleteBookEvent) = when(event){
+        is CompleteBookEvent.OpenAgonize ->{ openAgonizeActivity() }
+        is CompleteBookEvent.OpenBookReport ->{  }
+    }
+
+    private fun openAgonizeActivity(){
+        val intent = Intent(requireContext(), AgonizeHistoryActivity::class.java)
+            .putExtra(ReadingTapBookDialog.EXTRA_AGONIZE_BOOK,book)
+        startActivity(intent)
     }
 
 }
