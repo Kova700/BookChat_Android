7438e37761eff08cf8879717d5cdacc50e768f05
Tom <blak1002@gmail.com>
2022-12-04 13:21:52 +0900

    [feat] openAgonizeAcitivty in completeBookTapDialog

--- BookChat/app/src/main/java/com/example/bookchat/viewmodel/CompleteBookTapDialogViewModel.kt
+++ BookChat/app/src/main/java/com/example/bookchat/viewmodel/CompleteBookTapDialogViewModel.kt
@@ -1,11 +1,33 @@
 package com.example.bookchat.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.bookchat.data.BookShelfItem
 import com.example.bookchat.repository.BookRepository
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
 
 class CompleteBookTapDialogViewModel(private val bookRepository: BookRepository) : ViewModel() {
-    var starRating = MutableStateFlow<Float>(0.0F)
+    private val _eventFlow = MutableSharedFlow<CompleteBookEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
     lateinit var book: BookShelfItem
+
+    fun clickBookReportBtn(){
+        startEvent(CompleteBookEvent.OpenBookReport)
+    }
+
+    fun clickAgonizeBtn(){
+        startEvent(CompleteBookEvent.OpenAgonize)
+    }
+
+    private fun startEvent (event : CompleteBookEvent) = viewModelScope.launch {
+        _eventFlow.emit(event)
+    }
+
+    sealed class CompleteBookEvent {
+        object OpenBookReport : CompleteBookEvent()
+        object OpenAgonize :CompleteBookEvent()
+    }
 }
