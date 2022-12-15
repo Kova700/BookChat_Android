dd5ee08bdc885d9ba656c98d8186dbc9dc45d6cf
Tom <blak1002@gmail.com>
2022-12-15 23:48:30 +0900

    [feat] bookReport Exit dialog UI

--- BookChat/app/src/main/java/com/example/bookchat/ui/activity/BookReportActivity.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/activity/BookReportActivity.kt
@@ -1,6 +1,7 @@
 package com.example.bookchat.ui.activity
 
 import android.os.Bundle
+import androidx.activity.addCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -8,6 +9,7 @@ import androidx.lifecycle.lifecycleScope
 import com.example.bookchat.R
 import com.example.bookchat.data.BookShelfItem
 import com.example.bookchat.databinding.ActivityBookReportBinding
+import com.example.bookchat.ui.dialog.BookReportExitDialog
 import com.example.bookchat.ui.dialog.CompleteTapBookDialog.Companion.EXTRA_BOOKREPORT_BOOK
 import com.example.bookchat.viewmodel.BookReportViewModel
 import com.example.bookchat.viewmodel.BookReportViewModel.BookReportUIEvent
@@ -31,6 +33,7 @@ class BookReportActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this,R.layout.activity_book_report)
         binding.lifecycleOwner = this
         binding.viewmodel = bookReportViewModel
+        setBackPressedDispatcher()
 
         observeBookReportEvent()
     }
@@ -41,7 +44,7 @@ class BookReportActivity : AppCompatActivity() {
 
     private fun handleEvent(event : BookReportUIEvent){
         when(event){
-            is BookReportUIEvent.MoveToBack -> { finish() }
+            is BookReportUIEvent.MoveToBack -> { onBackPressedDispatcher.onBackPressed() }
         }
     }
 
@@ -49,9 +52,32 @@ class BookReportActivity : AppCompatActivity() {
         return intent.getSerializableExtra(EXTRA_BOOKREPORT_BOOK) as BookShelfItem
     }
 
+    private fun setBackPressedDispatcher(){
+        onBackPressedDispatcher.addCallback{
+            if (!bookReportViewModel.isEditingStatus()){
+                bookReportViewModel.initCachedData()
+                finish()
+                return@addCallback
+            }
+
+            if(bookReportViewModel.isNotChangedReport() || bookReportViewModel.isBookReportEmpty()) {
+                bookReportViewModel.initCachedData()
+                finish()
+                return@addCallback
+            }
+
+            val dialog = BookReportExitDialog()
+            dialog.show(this@BookReportActivity.supportFragmentManager, DIALOG_TAG_BOOK_REPORT_EXIT)
+        }
+    }
+
     override fun onDestroy() {
         bookReportViewModel.initCachedData()
         super.onDestroy()
+    }
+
+    companion object {
+        private const val DIALOG_TAG_BOOK_REPORT_EXIT = "BookReportExitDialog"
     }
 
 }
