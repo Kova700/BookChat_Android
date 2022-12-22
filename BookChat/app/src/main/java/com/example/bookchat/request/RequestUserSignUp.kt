cc50c81bcc923ced15ad4388a70dd7925ae8e337
Tom <blak1002@gmail.com>
2022-12-23 03:11:15 +0900

    [chore] modify UserSignUpDto, RequestUserSignUp class default value

--- BookChat/app/src/main/java/com/example/bookchat/request/RequestUserSignUp.kt
+++ BookChat/app/src/main/java/com/example/bookchat/request/RequestUserSignUp.kt
@@ -4,14 +4,15 @@ import com.example.bookchat.utils.OAuth2Provider
 import com.example.bookchat.utils.ReadingTaste
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
+import java.util.*
 
 data class RequestUserSignUp(
     @SerializedName("oauth2Provider")
     val oauth2Provider : OAuth2Provider,
     @SerializedName("nickname")
     var nickname: String,
-    @SerializedName("defaultProfileImageType")
-    val defaultProfileImageType : Int?,
     @SerializedName("readingTastes")
     var readingTastes : List<ReadingTaste>,
+    @SerializedName("defaultProfileImageType")
+    val defaultProfileImageType : Int = Random().nextInt(5) + 1,
 ) : Serializable
