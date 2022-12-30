ef55515c152fe301a2d8e6d4eb7381aad700dad7
Tom <blak1002@gmail.com>
2022-12-30 23:14:54 +0900

    [feat] getAgony API + UI logic

--- BookChat/app/src/main/java/com/example/bookchat/paging/AgonyPagingSource.kt
+++ BookChat/app/src/main/java/com/example/bookchat/paging/AgonyPagingSource.kt
@@ -4,11 +4,13 @@ import android.util.Log
 import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.example.bookchat.App
-import com.example.bookchat.data.*
-import com.example.bookchat.response.*
-import com.example.bookchat.utils.AgonyFolderHexColor
+import com.example.bookchat.data.Agony
+import com.example.bookchat.response.CursorMeta
+import com.example.bookchat.response.NetworkIsNotConnectedException
+import com.example.bookchat.response.ResponseBodyEmptyException
+import com.example.bookchat.response.ResponseGetAgony
 import com.example.bookchat.utils.Constants.TAG
-import com.example.bookchat.utils.StarRating
+import com.example.bookchat.utils.SearchSortOption
 import retrofit2.Response
 
 class AgonyPagingSource : PagingSource<Int,Agony>(){
@@ -18,43 +20,28 @@ class AgonyPagingSource : PagingSource<Int,Agony>(){
         if(!isNetworkConnected()) return LoadResult.Error(NetworkIsNotConnectedException())
         val page = params.key ?: STARTING_PAGE_INDEX
 
-        /*임시 더미 데이터----STRAT*/
-        val testAgonyList = listOf(
-            Agony(1L,"왜 계속 null safety 처리를 해 줘야 할까?",AgonyFolderHexColor.BLACK),
-            Agony(2L,"스코프 함수는 왜 쓰는 걸까?",AgonyFolderHexColor.MINT),
-            Agony(3L,"Flow는 왜 존재할까?",AgonyFolderHexColor.YELLOW),
-            Agony(4L,"Flow는 왜 존재할까?",AgonyFolderHexColor.GREEN),
-            Agony(5L,"Flow는 왜 존재할까?",AgonyFolderHexColor.ORANGE),
-            Agony(6L,"Flow는 왜 존재할까?",AgonyFolderHexColor.WHITE),
-            Agony(7L,"Flow는 왜 존재할까?",AgonyFolderHexColor.PURPLE)
-        )
-        val testMeta = CursorMeta(1,7,true,false,true,true,1)
-        return getLoadResult(testAgonyList, page, testMeta)
-        /*임시 더미 데이터----END*/
+        try {
+            response = App.instance.bookChatApiClient.getAgony(
+                size = params.loadSize.toString(),
+                postCursorId = page
+            )
+        }catch (e :Exception){
+            return LoadResult.Error(e)
+        }
 
-
-//        try {
-//            response = App.instance.bookChatApiClient.getAgony(
-//                size = params.loadSize.toString(),
-//                postCursorId = page
-//            )
-//        }catch (e :Exception){
-//            return LoadResult.Error(e)
-//        }
-//
-//        when(response.code()){
-//            200 -> {
-//                val result = response.body()
-//                result?.let {
-//                    val pagedAgony = result.agonyResponseList
-//                    Log.d(TAG, "AgonyPagingSource: load() - pagedAgony :$pagedAgony")
-//                    val meta = result.cursorMeta
-//                    return getLoadResult(pagedAgony, page, meta)
-//                }
-//                return LoadResult.Error(ResponseBodyEmptyException(response.errorBody()?.string()))
-//            }
-//            else -> return LoadResult.Error(Exception(createExceptionMessage(response.code(),response.errorBody()?.string())))
-//        }
+        when(response.code()){
+            200 -> {
+                val result = response.body()
+                result?.let {
+                    val pagedAgony = result.agonyResponseList
+                    Log.d(TAG, "AgonyPagingSource: load() - pagedAgony :$pagedAgony")
+                    val meta = result.cursorMeta
+                    return getLoadResult(pagedAgony, page, meta)
+                }
+                return LoadResult.Error(ResponseBodyEmptyException(response.errorBody()?.string()))
+            }
+            else -> return LoadResult.Error(Exception(createExceptionMessage(response.code(),response.errorBody()?.string())))
+        }
     }
 
     override fun getRefreshKey(state: PagingState<Int, Agony>): Int {
@@ -69,7 +56,7 @@ class AgonyPagingSource : PagingSource<Int,Agony>(){
         return try {
             LoadResult.Page(
                 data = data,
-                prevKey = if (nowPage == 1) null else nowPage - 1,
+                prevKey = if (nowPage == STARTING_PAGE_INDEX) null else nowPage - 1,
                 nextKey = getNextKey(nowPage, cursorMeta)
             )
         }catch (exception :Exception){
@@ -81,10 +68,10 @@ class AgonyPagingSource : PagingSource<Int,Agony>(){
         nowPage :Int,
         cursorMeta : CursorMeta
     ) :Int?{
-        if(!cursorMeta.hasNext) return null
+        if(cursorMeta.last) return null
 
         if (nowPage == STARTING_PAGE_INDEX){
-            return 4 * STARTING_PAGE_INDEX
+            return STARTING_PAGE_INDEX + 3
         }
         return nowPage + 1
     }
@@ -98,6 +85,6 @@ class AgonyPagingSource : PagingSource<Int,Agony>(){
     }
 
     companion object{
-        private const val STARTING_PAGE_INDEX = 1
+        private const val STARTING_PAGE_INDEX = 0
     }
 }
--- BookChat/app/src/main/java/com/example/bookchat/utils/SearchSortOption.kt
+++ BookChat/app/src/main/java/com/example/bookchat/utils/SearchSortOption.kt
@@ -2,7 +2,9 @@ package com.example.bookchat.utils
 
 import com.google.gson.annotations.SerializedName
 
-enum class SearchSortOption(option :String) {
-    @SerializedName("id,DESC") DESC("id,DESC"), //최신순
-    @SerializedName("id,ASC") ASC("id,ASC") //등록순
+enum class SearchSortOption {
+    @SerializedName("id,DESC")
+    DESC,  //최신순
+    @SerializedName("id,ASC")
+    ASC    //등록순
 }
