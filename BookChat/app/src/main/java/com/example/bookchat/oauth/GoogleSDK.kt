83ccb3a34543497a22e3365a636b3d4b325138ed
Tom <blak1002@gmail.com>
2023-02-15 06:37:46 +0900

    [feat] google login logic

---
+++ BookChat/app/src/main/java/com/example/bookchat/oauth/GoogleSDK.kt
@@ -0,0 +1,45 @@
+package com.example.bookchat.oauth
+
+import android.content.Context
+import android.content.Intent
+import com.example.bookchat.BuildConfig.GOOGLE_SERVER_CLIENT_ID
+import com.example.bookchat.data.IdToken
+import com.example.bookchat.data.response.NeedToGoogleLoginException
+import com.example.bookchat.data.response.TokenDoseNotExistException
+import com.example.bookchat.utils.DataStoreManager
+import com.example.bookchat.utils.OAuth2Provider
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+
+object GoogleSDK {
+
+    private val googleSignInOptions by lazy {
+        GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(GOOGLE_SERVER_CLIENT_ID)
+            .build()
+    }
+
+    suspend fun googleLogin(context :Context){
+        val userAccount = getUserGoogleAccount(context) ?: throw NeedToGoogleLoginException()
+        saveIdToken(userAccount.idToken )
+    }
+
+    private fun getUserGoogleAccount(context :Context): GoogleSignInAccount? =
+        GoogleSignIn.getLastSignedInAccount(context)
+
+    fun getSignInIntent(context :Context): Intent {
+        val mGoogleSignInClient = GoogleSignIn.getClient(context, googleSignInOptions)
+        return mGoogleSignInClient.signInIntent
+    }
+
+    private suspend fun saveIdToken(token : String?){
+        token ?: throw TokenDoseNotExistException()
+        DataStoreManager.saveIdToken(IdToken("Bearer ${token}", OAuth2Provider.GOOGLE) )
+    }
+
+    //로그아웃
+    //탈퇴
+}
--- BookChat/app/src/main/java/com/example/bookchat/viewmodel/LoginViewModel.kt
+++ BookChat/app/src/main/java/com/example/bookchat/viewmodel/LoginViewModel.kt
@@ -4,11 +4,9 @@ import android.content.Context
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.bookchat.data.response.ForbiddenException
-import com.example.bookchat.data.response.KakaoLoginFailException
-import com.example.bookchat.data.response.NeedToSignUpException
-import com.example.bookchat.data.response.NetworkIsNotConnectedException
-import com.example.bookchat.kakao.KakaoSDK
+import com.example.bookchat.data.response.*
+import com.example.bookchat.oauth.GoogleSDK
+import com.example.bookchat.oauth.KakaoSDK
 import com.example.bookchat.repository.UserRepository
 import com.example.bookchat.utils.Constants.TAG
 import com.example.bookchat.utils.DataStoreManager
@@ -19,9 +17,9 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor (
+class LoginViewModel @Inject constructor(
     private val userRepository: UserRepository
-    ) : ViewModel() {
+) : ViewModel() {
 
     private val _eventFlow = MutableSharedFlow<LoginEvent>()
     val eventFlow = _eventFlow.asSharedFlow()
@@ -47,14 +45,17 @@ class LoginViewModel @Inject constructor (
             .onFailure { failHandler(it) }
     }
 
+    fun startGoogleLogin(context: Context) = viewModelScope.launch {
+        Log.d(TAG, "LoginViewModel: requestGoogleLogin() - called")
+        runCatching { GoogleSDK.googleLogin(context) }
+            .onSuccess { bookchatLogin() }
+            .onFailure { failHandler(it) }
+    }
+
     fun bookchatLogin() = viewModelScope.launch {
         runCatching { userRepository.signIn() }
             .onSuccess { requestUserInfo() }
             .onFailure { failHandler(it) }
-    }
-
-    fun startGoogleLogin() = viewModelScope.launch {
-        Log.d(TAG, "LoginViewModel: requestGoogleLogin() - called")
     }
 
     private fun startEvent(event: LoginEvent) = viewModelScope.launch {
@@ -64,19 +65,21 @@ class LoginViewModel @Inject constructor (
     sealed class LoginEvent {
         object MoveToMain : LoginEvent()
         object MoveToSignUp : LoginEvent()
-        object Forbidden : LoginEvent()
+        object ForbiddenUser : LoginEvent()
         object NetworkError : LoginEvent()
-        object UnknownError : LoginEvent()
         object KakaoLoginFail : LoginEvent()
+        object NeedToGoogleLogin : LoginEvent()
+        object UnknownError : LoginEvent()
     }
 
     private fun failHandler(exception: Throwable) {
         Log.d(TAG, "LoginViewModel: failHandler() - exception : $exception")
         when (exception) {
             is NeedToSignUpException -> startEvent(LoginEvent.MoveToSignUp)
-            is ForbiddenException -> startEvent(LoginEvent.Forbidden)
+            is ForbiddenException -> startEvent(LoginEvent.ForbiddenUser)
             is NetworkIsNotConnectedException -> startEvent(LoginEvent.NetworkError)
             is KakaoLoginFailException -> startEvent(LoginEvent.KakaoLoginFail)
+            is NeedToGoogleLoginException -> startEvent(LoginEvent.NeedToGoogleLogin)
             else -> startEvent(LoginEvent.UnknownError)
         }
     }
