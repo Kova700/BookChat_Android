45f6773ab2cd4e34976a19c4b5b539824d55aa81
Tom <blak1002@gmail.com>
2023-02-19 15:06:00 +0900

    [feat] revise User profile logic

--- BookChat/app/src/main/java/com/example/bookchat/ui/activity/UserEditActivity.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/activity/UserEditActivity.kt
@@ -3,23 +3,29 @@ package com.example.bookchat.ui.activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.addCallback
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.lifecycleScope
 import com.example.bookchat.App
 import com.example.bookchat.R
-import com.example.bookchat.ui.activity.ImageCropActivity.Companion.EXTRA_USER_PROFILE_BYTE_ARRAY1
 import com.example.bookchat.databinding.ActivityUserEditBinding
+import com.example.bookchat.ui.activity.ImageCropActivity.Companion.EXTRA_USER_PROFILE_BYTE_ARRAY1
 import com.example.bookchat.utils.PermissionManager
+import com.example.bookchat.viewmodel.UserEditViewModel
+import com.example.bookchat.viewmodel.UserEditViewModel.UserEditUiEvent
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class UserEditActivity : AppCompatActivity() {
     private lateinit var permissionsLauncher : ActivityResultLauncher<Array<String>>
     private lateinit var binding :ActivityUserEditBinding
-    val userImage = MutableStateFlow(byteArrayOf())
+    private val userEditViewModel : UserEditViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,9 +33,11 @@ class UserEditActivity : AppCompatActivity() {
         permissionsLauncher = PermissionManager.getPermissionsLauncher(this)
         with(binding){
             lifecycleOwner = this@UserEditActivity
+            viewmodel = userEditViewModel
             activity = this@UserEditActivity
-            user = App.instance.getCachedUser()
         }
+        setBackPressedDispatcher()
+        observeUiEvent()
     }
 
     fun startUserProfileEdit(){
@@ -55,12 +63,31 @@ class UserEditActivity : AppCompatActivity() {
             if(result.resultCode == RESULT_OK){
                 val intent = result.data
                 val bitmapByteArray = intent?.getByteArrayExtra(EXTRA_USER_PROFILE_BYTE_ARRAY1) ?: byteArrayOf()
-                userImage.value = bitmapByteArray
+                userEditViewModel.newProfileImage.value = bitmapByteArray
             }
         }
 
-    fun clickBackBtn(){
+    private fun observeUiEvent() = lifecycleScope.launch{
+        userEditViewModel.eventFlow.collect{ event -> handleUiEvent(event)}
+    }
+
+    private fun setBackPressedDispatcher() {
+        onBackPressedDispatcher.addCallback {
+            userEditViewModel.saveChange()
+        }
+    }
+
+    private fun finishActivity(){
         finish()
+    }
+
+    private fun handleUiEvent(event: UserEditUiEvent) = when(event) {
+        is UserEditUiEvent.Finish -> { finishActivity() }
+        is UserEditUiEvent.UnknownError -> { makeToast(R.string.error_else) }
+    }
+
+    private fun makeToast(stringId :Int){
+        Toast.makeText(App.instance.applicationContext, stringId, Toast.LENGTH_SHORT).show()
     }
 
     companion object{
