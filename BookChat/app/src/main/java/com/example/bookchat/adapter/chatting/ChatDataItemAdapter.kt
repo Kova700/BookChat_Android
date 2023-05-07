3b23b869e22303f31f83266ebf540532336b5501
Tom <blak1002@gmail.com>
2023-05-07 14:49:20 +0900

    [fix] chatPagingAdapter returns the position value as -1

--- BookChat/app/src/main/java/com/example/bookchat/adapter/chatting/ChatDataItemAdapter.kt
+++ BookChat/app/src/main/java/com/example/bookchat/adapter/chatting/ChatDataItemAdapter.kt
@@ -3,47 +3,48 @@ package com.example.bookchat.adapter.chatting
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.paging.PagingDataAdapter
 import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bookchat.R
-import com.example.bookchat.data.Chat
+import com.example.bookchat.data.local.entity.ChatEntity
+import com.example.bookchat.data.local.entity.ChatEntity.ChatType
+import com.example.bookchat.databinding.EmptyLayoutBinding
 import com.example.bookchat.databinding.ItemChattingMineBinding
 import com.example.bookchat.databinding.ItemChattingNoticeBinding
 import com.example.bookchat.databinding.ItemChattingOtherBinding
 
 class ChatDataItemAdapter :
-    ListAdapter<Chat, RecyclerView.ViewHolder>(CHAT_ITEM_COMPARATOR) {
-
-    private lateinit var bindingChatMine: ItemChattingMineBinding
-    private lateinit var bindingChatOther: ItemChattingOtherBinding
-    private lateinit var bindingChatNotice: ItemChattingNoticeBinding
+    PagingDataAdapter<ChatEntity, RecyclerView.ViewHolder>(CHAT_ITEM_COMPARATOR) {
 
     inner class MineChatViewHolder(val binding: ItemChattingMineBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(chat: Chat) {
+        fun bind(chat: ChatEntity) {
             binding.chat = chat
         }
     }
 
     inner class OtherChatViewHolder(val binding: ItemChattingOtherBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(chat: Chat) {
+        fun bind(chat: ChatEntity) {
             binding.chat = chat
         }
     }
 
     inner class NoticeChatViewHolder(val binding: ItemChattingNoticeBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(chat: Chat) {
+        fun bind(chat: ChatEntity) {
             binding.chat = chat
         }
     }
 
+    inner class EmptyViewHolder(val binding: EmptyLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType) {
+        when (viewType) {
             R.layout.item_chatting_mine -> {
-                bindingChatMine = DataBindingUtil.inflate(
+                val bindingChatMine: ItemChattingMineBinding = DataBindingUtil.inflate(
                     LayoutInflater.from(parent.context),
                     R.layout.item_chatting_mine,
                     parent, false
@@ -51,7 +52,7 @@ class ChatDataItemAdapter :
                 return MineChatViewHolder(bindingChatMine)
             }
             R.layout.item_chatting_other -> {
-                bindingChatOther = DataBindingUtil.inflate(
+                val bindingChatOther: ItemChattingOtherBinding = DataBindingUtil.inflate(
                     LayoutInflater.from(parent.context),
                     R.layout.item_chatting_other,
                     parent, false
@@ -59,12 +60,20 @@ class ChatDataItemAdapter :
                 return OtherChatViewHolder(bindingChatOther)
             }
             R.layout.item_chatting_notice -> {
-                bindingChatNotice = DataBindingUtil.inflate(
+                val bindingChatNotice: ItemChattingNoticeBinding = DataBindingUtil.inflate(
                     LayoutInflater.from(parent.context),
                     R.layout.item_chatting_notice,
                     parent, false
                 )
                 return NoticeChatViewHolder(bindingChatNotice)
+            }
+            R.layout.empty_layout -> {
+                val bindingEmpty: EmptyLayoutBinding = DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.context),
+                    R.layout.empty_layout,
+                    parent, false
+                )
+                return EmptyViewHolder(bindingEmpty)
             }
             else -> throw RuntimeException("Received unknown ViewHolderType")
         }
@@ -72,21 +81,32 @@ class ChatDataItemAdapter :
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val currentItem = getItem(position)
-        when (currentItem.getChatType()) {
-            R.layout.item_chatting_mine -> (holder as MineChatViewHolder).bind(currentItem)
-            R.layout.item_chatting_other -> (holder as OtherChatViewHolder).bind(currentItem)
-            R.layout.item_chatting_notice -> (holder as NoticeChatViewHolder).bind(currentItem)
+        currentItem?.let {
+            when (currentItem.chatType) {
+                ChatType.Mine -> (holder as MineChatViewHolder).bind(currentItem)
+                ChatType.Other -> (holder as OtherChatViewHolder).bind(currentItem)
+                ChatType.Notice -> (holder as NoticeChatViewHolder).bind(currentItem)
+            }
         }
     }
 
-    override fun getItemViewType(position: Int): Int = getItem(position).getChatType()
+    override fun getItemViewType(position: Int): Int {
+        if (position == -1 || getItem(position) == null) return R.layout.empty_layout
+
+        return when (getItem(position)?.chatType) {
+            ChatType.Mine -> R.layout.item_chatting_mine
+            ChatType.Other -> R.layout.item_chatting_other
+            ChatType.Notice -> R.layout.item_chatting_notice
+            else -> throw RuntimeException("Received unknown ChatType")
+        }
+    }
 
     companion object {
-        val CHAT_ITEM_COMPARATOR = object : DiffUtil.ItemCallback<Chat>() {
-            override fun areItemsTheSame(oldItem: Chat, newItem: Chat) =
+        val CHAT_ITEM_COMPARATOR = object : DiffUtil.ItemCallback<ChatEntity>() {
+            override fun areItemsTheSame(oldItem: ChatEntity, newItem: ChatEntity) =
                 oldItem.chatId == newItem.chatId
 
-            override fun areContentsTheSame(oldItem: Chat, newItem: Chat) =
+            override fun areContentsTheSame(oldItem: ChatEntity, newItem: ChatEntity) =
                 oldItem == newItem
         }
     }
