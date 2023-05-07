ea09c3ebba042d16dea11a91254ff952e479add6
Tom <blak1002@gmail.com>
2023-05-07 14:54:04 +0900

    [chore] add userId property in User data class

--- BookChat/app/src/main/java/com/example/bookchat/data/User.kt
+++ BookChat/app/src/main/java/com/example/bookchat/data/User.kt
@@ -3,8 +3,9 @@ package com.example.bookchat.data
 import com.example.bookchat.utils.UserDefaultProfileImageType
 import com.google.gson.annotations.SerializedName
 
-//UserID가 없음 (서버와 협의)
 data class User(
+    @SerializedName("userId")
+    val userId : Long,
     @SerializedName("userNickname")
     val userNickname: String,
     @SerializedName("userEmail")
