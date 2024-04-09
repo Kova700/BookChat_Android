6e851c80b322325c7d9dcb4db02e6f15b671cd44
Tom <blak1002@gmail.com>
2024-04-09 23:07:14 +0900

    [revise] SearchTapDefaultFragment Fragment Binding 변수 초기화 방식 수정

--- BookChat/app/src/main/java/com/example/bookchat/ui/search/SearchTapDefaultFragment.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/search/SearchTapDefaultFragment.kt
@@ -9,17 +9,28 @@ import androidx.fragment.app.Fragment
 import com.example.bookchat.R
 import com.example.bookchat.databinding.FragmentSearchTapDefaultBinding
 
-/*추천 검색어 추가 예정*/
+/*TODO : 추천 검색어 추가 예정*/
 class SearchTapDefaultFragment : Fragment() {
-    private lateinit var binding : FragmentSearchTapDefaultBinding
+	private var _binding: FragmentSearchTapDefaultBinding? = null
+	private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search_tap_default, container, false)
-        return binding.root
-    }
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		_binding =
+			DataBindingUtil.inflate(inflater, R.layout.fragment_search_tap_default, container, false)
+		return binding.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+	}
+
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
+	}
 
 }
