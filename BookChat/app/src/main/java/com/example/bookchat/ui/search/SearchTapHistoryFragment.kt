3e62e8d27936c6d01c72c814b324b193933b520d
Tom <blak1002@gmail.com>
2024-04-16 23:26:40 +0900

    [refector(SearchTapHistory)] 검색 기록 관련 데이터 흐름 변경 + ListAdpater로 수정

--- BookChat/app/src/main/java/com/example/bookchat/ui/search/SearchTapHistoryFragment.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/search/SearchTapHistoryFragment.kt
@@ -4,66 +4,72 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bookchat.R
-import com.example.bookchat.ui.search.adapter.searchhistory.SearchHistoryAdapter
 import com.example.bookchat.databinding.FragmentSearchTapHistoryBinding
-import com.example.bookchat.utils.DataStoreManager
+import com.example.bookchat.ui.search.adapter.searchhistory.SearchHistoryAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-/*추후 확장이 필요하다 느끼면 ViewModel로 로직 이동*/
 @AndroidEntryPoint
 class SearchTapHistoryFragment : Fragment() {
-    private lateinit var binding : FragmentSearchTapHistoryBinding
-    private lateinit var searchHistoryAdapter : SearchHistoryAdapter
-    private val searchViewModel: SearchViewModel by viewModels( { requireParentFragment() } )
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search_tap_history, container, false)
-        binding.fragment = this
-        roadHistory()
-        return binding.root
-    }
+	private var _binding: FragmentSearchTapHistoryBinding? = null
+	private val binding get() = _binding!!
 
-    private fun roadHistory() = lifecycleScope.launch{
-        val historyList = DataStoreManager.getSearchHistory() ?: mutableListOf()
-        initAdapter(historyList)
-        initRcv()
-    }
+	private val searchViewModel by viewModels<SearchViewModel>({ requireParentFragment() })
 
-    private fun initAdapter(historyList :MutableList<String>) {
-        searchHistoryAdapter = SearchHistoryAdapter(historyList)
-        searchHistoryAdapter.setHasStableIds(true)
-        searchHistoryAdapter.setItemClickListener(object : SearchHistoryAdapter.OnItemClickListener {
-            override fun onClick(v: View) {
-                val keyword = (v as TextView).text.toString()
-                searchViewModel.clickHistory(keyword)
-            }
-        })
-    }
+	@Inject
+	lateinit var searchHistoryAdapter: SearchHistoryAdapter
 
-    private fun initRcv() {
-        with(binding){
-            historyRcv.adapter = searchHistoryAdapter
-            historyRcv.setHasFixedSize(true)
-            historyRcv.layoutManager = LinearLayoutManager(requireContext())
-        }
-    }
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		_binding = DataBindingUtil.inflate(
+			inflater, R.layout.fragment_search_tap_history, container, false
+		)
+		binding.viewmodel = searchViewModel
+		return binding.root
+	}
 
-    fun clearHistory() = lifecycleScope.launch {
-        DataStoreManager.clearSearchHistory()
-        searchHistoryAdapter.searchHistoryList = DataStoreManager.getSearchHistory() ?: mutableListOf()
-        searchHistoryAdapter.notifyDataSetChanged()
-    }
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		initAdapter()
+		initRcv()
+		observeUiState()
+	}
 
+	override fun onDestroyView() {
+		super.onDestroyView()
+		_binding = null
+	}
+
+	private fun observeUiState() = viewLifecycleOwner.lifecycleScope.launch {
+		searchViewModel.uiState.collect { state ->
+			searchHistoryAdapter.submitList(state.searchHistory)
+		}
+	}
+
+	private fun initAdapter() {
+		searchHistoryAdapter.onItemClick = { position ->
+			searchViewModel.onClickSearchHistory(position)
+		}
+		searchHistoryAdapter.onDeleteBtnClick = { position ->
+			searchViewModel.onClickSearchHistoryDeleteBtn(position)
+		}
+	}
+
+	private fun initRcv() {
+		with(binding.historyRcv) {
+			adapter = searchHistoryAdapter
+			layoutManager = LinearLayoutManager(requireContext())
+		}
+	}
 }
--- BookChat/app/src/main/java/com/example/bookchat/ui/search/adapter/searchhistory/SearchHistoryAdapter.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/search/adapter/searchhistory/SearchHistoryAdapter.kt
@@ -1,64 +1,60 @@
 package com.example.bookchat.ui.search.adapter.searchhistory
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bookchat.R
 import com.example.bookchat.databinding.ItemSearchHistoryBinding
-import com.example.bookchat.utils.DataStoreManager
-import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
 
-class SearchHistoryAdapter(var searchHistoryList: MutableList<String>) :
-    RecyclerView.Adapter<SearchHistoryAdapter.SearchHistoryViewHolder>() {
+class SearchHistoryAdapter @Inject constructor() :
+	ListAdapter<String, SearchHistoryViewHolder>(SEARCH_HISTORY_COMPARATOR) {
 
-    private lateinit var binding: ItemSearchHistoryBinding
-    private lateinit var itemClickListener: OnItemClickListener
+	var onItemClick: ((Int) -> Unit)? = null
+	var onDeleteBtnClick: ((Int) -> Unit)? = null
 
-    inner class SearchHistoryViewHolder(val binding: ItemSearchHistoryBinding) :
-        RecyclerView.ViewHolder(binding.root){
-        fun bind(historyKeyword : String){
-            binding.historyKeyword = historyKeyword
-            this.binding.searchHistoryTv.setOnClickListener {
-                itemClickListener.onClick(it)
-            }
-            this.binding.deleteSearchHistoryBtn.setOnClickListener{
-                searchHistoryList.removeAt(this.absoluteAdapterPosition)
-                overWriteHistory(searchHistoryList)
-                this@SearchHistoryAdapter.notifyDataSetChanged()
-            }
-        }
-    }
+	override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchHistoryViewHolder {
+		val binding: ItemSearchHistoryBinding = DataBindingUtil.inflate(
+			LayoutInflater.from(parent.context), R.layout.item_search_history,
+			parent, false
+		)
+		return SearchHistoryViewHolder(binding, onItemClick, onDeleteBtnClick)
+	}
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchHistoryViewHolder {
-        binding = DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.item_search_history, parent, false)
-        return SearchHistoryViewHolder(binding)
-    }
+	override fun onBindViewHolder(holder: SearchHistoryViewHolder, position: Int) {
+		holder.bind(getItem(position))
+	}
 
-    override fun onBindViewHolder(holder: SearchHistoryViewHolder, position: Int) {
-        holder.bind(searchHistoryList[position])
-    }
+	companion object {
+		val SEARCH_HISTORY_COMPARATOR = object : DiffUtil.ItemCallback<String>() {
+			override fun areItemsTheSame(oldItem: String, newItem: String) =
+				oldItem == newItem
 
-    private fun overWriteHistory(historyList: List<String>) = runBlocking {
-        DataStoreManager.overWriteHistory(historyList)
-    }
+			override fun areContentsTheSame(oldItem: String, newItem: String) =
+				oldItem == newItem
+		}
+	}
+}
 
-    //notifyDataSetChanged()시에 수정된 ItemView구분을 위해 사용 (수정된 ItemView만 갱신)
-    //ListAdapter와 DiffUtil사용하면 굳이 사용하지 않아도 됌
-    override fun getItemId(position: Int): Long {
-        return position.toLong()
-    }
+class SearchHistoryViewHolder(
+	private val binding: ItemSearchHistoryBinding,
+	private val onItemClick: ((Int) -> Unit)?,
+	private val onDeleteBtnClick: ((Int) -> Unit)?
+) : RecyclerView.ViewHolder(binding.root) {
+	init {
+		binding.searchHistoryTv.setOnClickListener {
+			onItemClick?.invoke(absoluteAdapterPosition)
+		}
 
-    override fun getItemCount(): Int {
-        return searchHistoryList.size
-    }
+		binding.deleteSearchHistoryBtn.setOnClickListener {
+			onDeleteBtnClick?.invoke(absoluteAdapterPosition)
+		}
+	}
 
-    interface OnItemClickListener {
-        fun onClick(v: View)
-    }
-
-    fun setItemClickListener(onItemClickListener: OnItemClickListener) {
-        this.itemClickListener = onItemClickListener
-    }
+	fun bind(historyKeyword: String) {
+		binding.historyKeyword = historyKeyword
+	}
 }
