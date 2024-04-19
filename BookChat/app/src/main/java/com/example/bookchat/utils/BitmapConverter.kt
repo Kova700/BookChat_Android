3c904e4c268bbd29dc4ab057a0c5d168d0240060
Tom <blak1002@gmail.com>
2024-04-19 16:05:16 +0900

    [refector(BitmapConverter)] 함수 default value형식으로 수정 + 불필요한 함수 제거

--- BookChat/app/src/main/java/com/example/bookchat/utils/BitmapConverter.kt
+++ BookChat/app/src/main/java/com/example/bookchat/utils/BitmapConverter.kt
@@ -1,22 +1,17 @@
 package com.example.bookchat.utils
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Build
 import java.io.ByteArrayOutputStream
 
-fun Bitmap.compressToByteArray(): ByteArray {
+fun Bitmap.compressToByteArray(compressQuality: Int = BITMAP_COMPRESS_QUALITY): ByteArray {
 	val stream = ByteArrayOutputStream()
 	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-		this.compress(Bitmap.CompressFormat.WEBP_LOSSLESS, BITMAP_COMPRESS_QUALITY, stream)
+		this.compress(Bitmap.CompressFormat.WEBP_LOSSLESS, compressQuality, stream)
 		return stream.toByteArray()
 	}
 	this.compress(Bitmap.CompressFormat.WEBP, BITMAP_COMPRESS_QUALITY, stream)
 	return stream.toByteArray()
-}
-
-fun ByteArray.toBitmap(): Bitmap {
-	return BitmapFactory.decodeByteArray(this, 0, this.size)
 }
 
 private const val BITMAP_COMPRESS_QUALITY = 80 //임시 80
