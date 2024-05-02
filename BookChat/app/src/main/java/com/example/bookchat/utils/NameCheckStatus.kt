42cf4dc9a1cf783d6cef20df41381a612ddaeed0
Tom <blak1002@gmail.com>
2024-05-02 18:25:39 +0900

    [refector] NameCheckStatus domain 레이어로 이동

--- BookChat/app/src/main/java/com/example/bookchat/utils/NameCheckStatus.kt
+++
@@ -1,9 +0,0 @@
-package com.example.bookchat.utils
-
-sealed class NameCheckStatus {
-    object Default :NameCheckStatus()
-    object IsShort :NameCheckStatus()
-    object IsDuplicate :NameCheckStatus()
-    object IsSpecialCharInText :NameCheckStatus()
-    object IsPerfect :NameCheckStatus()
-}
