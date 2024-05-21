44d64bb2d933a4513f7776e682b9ae967f945d77
Tom <blak1002@gmail.com>
2024-05-22 00:19:25 +0900

    [refector(FcmMessage)] channelId 추가 + 변경된 반환값에 맞게 타입 수정

--- BookChat/app/src/main/java/com/example/bookchat/data/network/model/response/FcmMessage.kt
+++ BookChat/app/src/main/java/com/example/bookchat/data/network/model/response/FcmMessage.kt
@@ -3,9 +3,16 @@ package com.example.bookchat.data.network.model.response
 import com.example.bookchat.domain.model.PushType
 import com.google.gson.annotations.SerializedName
 
-data class FCMPushMessage(
+data class FcmMessage(
 	@SerializedName("pushType")
 	val pushType: PushType,
 	@SerializedName("body")
+	val body: FcmBody,
+)
+
+data class FcmBody(
+	@SerializedName("chatId")
 	val chatId: Long,
+	@SerializedName("chatRoomId")
+	val channelId: Long,
 )
