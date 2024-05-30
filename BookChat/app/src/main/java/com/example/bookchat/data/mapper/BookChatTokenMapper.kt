a555921f63c81a7fde29d30b52fe18baccade901
Tom <blak1002@gmail.com>
2024-05-30 15:05:09 +0900

    [chore] 불필요한 import 경로문 제거

--- BookChat/app/src/main/java/com/example/bookchat/data/mapper/BookChatTokenMapper.kt
+++ BookChat/app/src/main/java/com/example/bookchat/data/mapper/BookChatTokenMapper.kt
@@ -3,6 +3,6 @@ package com.example.bookchat.data.mapper
 import com.example.bookchat.data.network.model.response.BookChatTokenResponse
 import com.example.bookchat.domain.model.BookChatToken
 
-fun com.example.bookchat.data.network.model.response.BookChatTokenResponse.toBookChatToken(): BookChatToken {
+fun BookChatTokenResponse.toBookChatToken(): BookChatToken {
 	return BookChatToken(accessToken, refreshToken)
 }
