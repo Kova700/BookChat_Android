2bfce3629db37f874f051ffe21e68495efc34cd8
Tom <blak1002@gmail.com>
2024-06-04 18:12:57 +0900

    [style(AgonyMapper)] 불필요한 import문 제거

--- BookChat/app/src/main/java/com/example/bookchat/data/mapper/AgonyMapper.kt
+++ BookChat/app/src/main/java/com/example/bookchat/data/mapper/AgonyMapper.kt
@@ -5,7 +5,7 @@ import com.example.bookchat.data.network.model.response.AgonyResponse
 import com.example.bookchat.domain.model.Agony
 import com.example.bookchat.domain.model.AgonyFolderHexColor
 
-fun com.example.bookchat.data.network.model.response.AgonyResponse.toAgony(): Agony {
+fun AgonyResponse.toAgony(): Agony {
 	return Agony(
 		agonyId = agonyId,
 		title = title,
@@ -13,28 +13,28 @@ fun com.example.bookchat.data.network.model.response.AgonyResponse.toAgony(): Ag
 	)
 }
 
-fun List<com.example.bookchat.data.network.model.response.AgonyResponse>.toAgony() = this.map { it.toAgony() }
+fun List<AgonyResponse>.toAgony() = this.map { it.toAgony() }
 
-fun AgonyFolderHexColor.toNetWork(): com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork {
+fun AgonyFolderHexColor.toNetWork(): AgonyFolderHexColorNetwork {
 	return when (this) {
-		AgonyFolderHexColor.WHITE -> com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.WHITE
-		AgonyFolderHexColor.BLACK -> com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.BLACK
-		AgonyFolderHexColor.PURPLE -> com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.PURPLE
-		AgonyFolderHexColor.MINT -> com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.MINT
-		AgonyFolderHexColor.GREEN -> com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.GREEN
-		AgonyFolderHexColor.YELLOW -> com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.YELLOW
-		AgonyFolderHexColor.ORANGE -> com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.ORANGE
+		AgonyFolderHexColor.WHITE -> AgonyFolderHexColorNetwork.WHITE
+		AgonyFolderHexColor.BLACK -> AgonyFolderHexColorNetwork.BLACK
+		AgonyFolderHexColor.PURPLE -> AgonyFolderHexColorNetwork.PURPLE
+		AgonyFolderHexColor.MINT -> AgonyFolderHexColorNetwork.MINT
+		AgonyFolderHexColor.GREEN -> AgonyFolderHexColorNetwork.GREEN
+		AgonyFolderHexColor.YELLOW -> AgonyFolderHexColorNetwork.YELLOW
+		AgonyFolderHexColor.ORANGE -> AgonyFolderHexColorNetwork.ORANGE
 	}
 }
 
-fun com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.toAgony(): AgonyFolderHexColor {
+fun AgonyFolderHexColorNetwork.toAgony(): AgonyFolderHexColor {
 	return when (this) {
-		com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.WHITE -> AgonyFolderHexColor.WHITE
-		com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.BLACK -> AgonyFolderHexColor.BLACK
-		com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.PURPLE -> AgonyFolderHexColor.PURPLE
-		com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.MINT -> AgonyFolderHexColor.MINT
-		com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.GREEN -> AgonyFolderHexColor.GREEN
-		com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.YELLOW -> AgonyFolderHexColor.YELLOW
-		com.example.bookchat.data.network.model.AgonyFolderHexColorNetwork.ORANGE -> AgonyFolderHexColor.ORANGE
+		AgonyFolderHexColorNetwork.WHITE -> AgonyFolderHexColor.WHITE
+		AgonyFolderHexColorNetwork.BLACK -> AgonyFolderHexColor.BLACK
+		AgonyFolderHexColorNetwork.PURPLE -> AgonyFolderHexColor.PURPLE
+		AgonyFolderHexColorNetwork.MINT -> AgonyFolderHexColor.MINT
+		AgonyFolderHexColorNetwork.GREEN -> AgonyFolderHexColor.GREEN
+		AgonyFolderHexColorNetwork.YELLOW -> AgonyFolderHexColor.YELLOW
+		AgonyFolderHexColorNetwork.ORANGE -> AgonyFolderHexColor.ORANGE
 	}
 }
