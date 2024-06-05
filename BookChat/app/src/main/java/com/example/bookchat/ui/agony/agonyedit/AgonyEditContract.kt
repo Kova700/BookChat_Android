fb5e074254d9af97df311625909829cf67ea0801
Tom <blak1002@gmail.com>
2024-06-05 11:36:53 +0900

    [refector(AgonyEdit)] View와 비즈니스 로직 분리

---
+++ BookChat/app/src/main/java/com/example/bookchat/ui/agony/agonyedit/AgonyEditContract.kt
@@ -0,0 +1,37 @@
+package com.example.bookchat.ui.agony.agonyedit
+
+import com.example.bookchat.domain.model.Agony
+
+data class AgonyEditUiState(
+	val uiState: UiState,
+	val newTitle: String,
+	val agony: Agony,
+) {
+
+	val isPossibleChangeAgony
+		get() = (agony.title != newTitle)
+						&& newTitle.isNotBlank()
+
+	enum class UiState {
+		SUCCESS,
+		LOADING,
+		ERROR,
+	}
+
+	companion object {
+		val DEFAULT = AgonyEditUiState(
+			uiState = UiState.SUCCESS,
+			newTitle = "",
+			agony = Agony.DEFAULT
+		)
+	}
+}
+
+sealed class AgonyEditUiEvent {
+	object MoveToBack : AgonyEditUiEvent()
+
+	data class MakeToast(
+		val stringId: Int,
+	) : AgonyEditUiEvent()
+
+}
