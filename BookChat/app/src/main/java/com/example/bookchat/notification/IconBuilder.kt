74e64c76714098b146beba65f7d626a68717151c
Tom <blak1002@gmail.com>
2024-07-07 19:04:29 +0900

    [feat + refactor] IconBuilder 이미지 스쿼클 모양으로 수정 + Glide로 bitmap 추출로 리팩토링

--- BookChat/app/src/main/java/com/example/bookchat/notification/IconBuilder.kt
+++ BookChat/app/src/main/java/com/example/bookchat/notification/IconBuilder.kt
@@ -1,40 +1,44 @@
 package com.example.bookchat.notification
 
 import android.content.Context
-import android.graphics.BitmapFactory
 import androidx.core.graphics.drawable.IconCompat
-import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
-import androidx.core.graphics.drawable.toBitmapOrNull
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.example.bookchat.utils.dpToPx
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.net.URL
 import javax.inject.Inject
 
 interface IconBuilder {
 	suspend fun buildIcon(imageUrl: String?): IconCompat?
 }
 
-//TODO : 이미지 load 라이브러리로 비트맵 생성되게 변경 (캐시 메모리 활요을 위해)
-//TODO : 스쿼클 모양으로 수정
 class IconBuilderImpl @Inject constructor(
 	@ApplicationContext private val context: Context,
 ) : IconBuilder {
 	override suspend fun buildIcon(imageUrl: String?): IconCompat? {
 		if (imageUrl.isNullOrBlank()) return null
 
+		val imageSize = 35.dpToPx(context)
+		val roundedCornersRadiusPx = 14.dpToPx(context)
+
 		return withContext(Dispatchers.IO) {
 			runCatching {
-				URL(imageUrl).openStream().use {
-					RoundedBitmapDrawableFactory.create(
-						context.resources,
-						BitmapFactory.decodeStream(it),
+				Glide.with(context)
+					.asBitmap()
+					.load(imageUrl)
+					.apply(
+						RequestOptions
+							.overrideOf(imageSize)
+							.transform(RoundedCorners(roundedCornersRadiusPx))
 					)
-						.apply { isCircular = true }
-						.toBitmapOrNull()
-				}
+					.submit()
+					.get()
 					?.let(IconCompat::createWithBitmap)
 			}.getOrNull()
 		}
 	}
+
 }
