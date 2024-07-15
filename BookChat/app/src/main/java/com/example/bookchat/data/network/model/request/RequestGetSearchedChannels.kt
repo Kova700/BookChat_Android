e40a3271863bcceb703d707529c43a1fd340a682
Tom <blak1002@gmail.com>
2024-07-15 16:45:49 +0900

    [chore(RequestGetSearchedChannels)] 가변타입 변수 읽기전용 타입으로 수정

--- BookChat/app/src/main/java/com/example/bookchat/data/network/model/request/RequestGetSearchedChannels.kt
+++ BookChat/app/src/main/java/com/example/bookchat/data/network/model/request/RequestGetSearchedChannels.kt
@@ -1,8 +1,8 @@
 package com.example.bookchat.data.network.model.request
 
 data class RequestGetSearchedChannels(
-    var roomName: String? = null,
-    var title: String? = null,
-    var isbn: String? = null,
-    var tags: String? = null,
+	val roomName: String? = null,
+	val title: String? = null,
+	val isbn: String? = null,
+	val tags: String? = null,
 )
