894e237851322b0d95f3e10c4a420ef98b53300f
Tom <blak1002@gmail.com>
2024-07-30 15:24:59 +0900

    [feat(Sanckbar)] 스낵바 커스텀 완료

--- BookChat/app/src/main/java/com/example/bookchat/utils/SnackBarUtil.kt
+++ BookChat/app/src/main/java/com/example/bookchat/utils/SnackBarUtil.kt
@@ -1,12 +1,36 @@
 package com.example.bookchat.utils
 
+import android.view.Gravity
 import android.view.View
+import android.widget.FrameLayout
+import androidx.appcompat.content.res.AppCompatResources
+import com.example.bookchat.R
+import com.google.android.material.snackbar.BaseTransientBottomBar.ANIMATION_MODE_SLIDE
 import com.google.android.material.snackbar.Snackbar
 
 fun View.showSnackBar(textId: Int) {
-	Snackbar.make(this, textId, Snackbar.LENGTH_SHORT).show()
+	getSnackBar(context.getString(textId)).show()
 }
 
 fun View.showSnackBar(text: String) {
-	Snackbar.make(this, text, Snackbar.LENGTH_SHORT).show()
+	getSnackBar(text).show()
+}
+
+private fun View.getSnackBar(text: String): Snackbar {
+	return Snackbar
+		.make(this, text, Snackbar.LENGTH_SHORT)
+		.setAnchorView(this)
+		.apply {
+			animationMode = ANIMATION_MODE_SLIDE
+			view.background = AppCompatResources.getDrawable(context, R.drawable.snackbar_background)
+			setTextColor(context.getColor(R.color.white))
+			view.layoutParams = view.getSnackBarLayoutParam()
+		}
+}
+
+private fun View.getSnackBarLayoutParam(): FrameLayout.LayoutParams {
+	return (layoutParams as (FrameLayout.LayoutParams)).apply {
+		gravity = Gravity.BOTTOM or Gravity.CENTER
+		width = FrameLayout.LayoutParams.WRAP_CONTENT
+	}
 }
