7bb474963f4a8d0307883d81b300f807d8289fdd
Tom <blak1002@gmail.com>
2024-08-01 20:09:52 +0900

    [refactor(CompleteBookStarSetDialog)] 데이터 바인딩 제거

--- BookChat/app/src/main/java/com/example/bookchat/ui/search/dialog/CompleteBookStarSetDialog.kt
+++ BookChat/app/src/main/java/com/example/bookchat/ui/search/dialog/CompleteBookStarSetDialog.kt
@@ -6,10 +6,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
-import com.example.bookchat.R
 import com.example.bookchat.databinding.DialogCompleteBookSetStarsBinding
 
 class CompleteBookStarSetDialog : DialogFragment() {
@@ -24,19 +22,16 @@ class CompleteBookStarSetDialog : DialogFragment() {
 	override fun onCreateView(
 		inflater: LayoutInflater,
 		container: ViewGroup?,
-		savedInstanceState: Bundle?
+		savedInstanceState: Bundle?,
 	): View {
-		_binding =
-			DataBindingUtil.inflate(inflater, R.layout.dialog_complete_book_set_stars, container, false)
-		binding.lifecycleOwner = this
-		binding.dialog = this
+		_binding = DialogCompleteBookSetStarsBinding.inflate(inflater, container, false)
 		return binding.root
 	}
 
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 		dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-		initScaleRatingBar()
+		initViewState()
 	}
 
 	override fun onDestroyView() {
@@ -44,18 +39,24 @@ class CompleteBookStarSetDialog : DialogFragment() {
 		_binding = null
 	}
 
+	private fun initViewState() {
+		initScaleRatingBar()
+		binding.cancelBtn.setOnClickListener { onClickCancelBtn() }
+		binding.completeBtn.setOnClickListener { onClickCompleteOkBtn() }
+	}
+
 	private fun initScaleRatingBar() {
 		binding.starRatingBar.setOnRatingChangeListener { _, rating, _ ->
-			searchBookDialogViewModel.onStarRatingChange(rating)
+			searchBookDialogViewModel.onChangeStarRating(rating)
 		}
 	}
 
 	fun onClickCancelBtn() {
-		this.dismiss()
+		dismiss()
 	}
 
 	fun onClickCompleteOkBtn() {
 		searchBookDialogViewModel.onClickCompleteOkBtn()
-		this.dismiss()
+		dismiss()
 	}
 }
